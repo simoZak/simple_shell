6ab310cc19624a63485275a34245d0d8622555c5
simoZak <zakhnini92@gmail.com>
2023-05-25 22:22:50 +0100

    task 0

--- main.c
+++ main.c
@@ -11,43 +11,44 @@
 
 int main(__attribute__((unused))int ac, char **argv, char **env)
 {
-	char *lineptr = NULL, *line_cpy = NULL, *token = NULL;
-	size_t n = 0;
-	ssize_t get;
-	const char *delim = " \n";
-	int ntok = 0, i;
+        char *lineptr = NULL, *line_cpy = NULL, *token = NULL;
+        size_t n = 0;
+        ssize_t get;
+        const char *delim = " \n";
+        int ntok = 0, i;
 
-	while (1)
-	{
-		get = getline(&lineptr, &n, stdin);
-		if (get == -1)
-		{
-			free(lineptr);
-			exit(EXIT_SUCCESS);
-		}
-		line_cpy = malloc((sizeof(char) * get + 1));
-		str_cpy(line_cpy, lineptr);
-		token = strtok(lineptr, delim);
-		while (token != NULL)
-		{
-			ntok++;
-			token = strtok(NULL, delim);
-		}
-		free(token);
-		ntok++;
-		argv = malloc(sizeof(char *) * ntok);
-		token = strtok(line_cpy, delim);
-		for (i = 0; token != NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char *) * _strlen(token));
-			str_cpy(argv[i], token);
-			token = strtok(NULL, delim);
-		}
-		free(token);
-		argv[i] = NULL;
-		execute_command(argv, env);
-	}
-	free(lineptr);
-	free(line_cpy);
-	return (0);
+        while (1)
+        {
+                get = getline(&lineptr, &n, stdin);
+                if (get == -1)
+                {
+                        free(lineptr);
+                        exit(EXIT_SUCCESS);
+                }
+                line_cpy = malloc((sizeof(char) * get + 1));
+                str_cpy(line_cpy, lineptr);
+                token = strtok(lineptr, delim);
+                while (token != NULL)
+                {
+                        ntok++;
+                        token = strtok(NULL, delim);
+                }
+                free(token);
+                ntok++;
+                argv = malloc(sizeof(char *) * ntok);
+                token = strtok(line_cpy, delim);
+                for (i = 0; token != NULL; i++)
+                {
+                        argv[i] = malloc(sizeof(char *) * _strlen(token));
+                        str_cpy(argv[i], token);
+                        token = strtok(NULL, delim);
+                }
+                free(token);
+                argv[i] = NULL;
+                execute_command(argv, env);
+        }
+        free(lineptr);
+        free(line_cpy);
+        return (0);
 }
+
