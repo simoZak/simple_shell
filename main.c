c5558adadfbda5591ee69efda7bc2dfd18177f4c
simoZak <zakhnini92@gmail.com>
2023-05-25 22:34:55 +0100

    task 0

--- main.c
+++ main.c
@@ -9,46 +9,68 @@
  * Return: 0 on success.
  */
 
-int main(__attribute__((unused))int ac, char **argv, char **env)
-{
-        char *lineptr = NULL, *line_cpy = NULL, *token = NULL;
-        size_t n = 0;
-        ssize_t get;
-        const char *delim = " \n";
-        int ntok = 0, i;
+int main(int ac, char **argv){
+    char *prompt = "(Eshell) $ ";
+    char *lineptr = NULL, *lineptr_copy = NULL;
+    size_t n = 0;
+    ssize_t nchars_read;
+    const char *delim = " \n";
+    int num_tokens = 0;
+    char *token;
+    int i;
 
-        while (1)
-        {
-                get = getline(&lineptr, &n, stdin);
-                if (get == -1)
-                {
-                        free(lineptr);
-                        exit(EXIT_SUCCESS);
-                }
-                line_cpy = malloc((sizeof(char) * get + 1));
-                str_cpy(line_cpy, lineptr);
-                token = strtok(lineptr, delim);
-                while (token != NULL)
-                {
-                        ntok++;
-                        token = strtok(NULL, delim);
-                }
-                free(token);
-                ntok++;
-                argv = malloc(sizeof(char *) * ntok);
-                token = strtok(line_cpy, delim);
-                for (i = 0; token != NULL; i++)
-                {
-                        argv[i] = malloc(sizeof(char *) * _strlen(token));
-                        str_cpy(argv[i], token);
-                        token = strtok(NULL, delim);
-                }
-                free(token);
-                argv[i] = NULL;
-                execute_command(argv, env);
+    /* declaring void variables */
+    (void)ac;
+
+    /* A loop for the shell's prompt */
+    while (1) {
+        printf("%s", prompt);
+        nchars_read = getline(&lineptr, &n, stdin);
+        /* check if the getline function woked or failed */ 
+        if (nchars_read == -1){
+            printf("Exiting shell....\n");
+            return (-1);
         }
-        free(lineptr);
-        free(line_cpy);
-        return (0);
-}
 
+        /* allocate space to copy the lineptr */
+        lineptr_copy = malloc(sizeof(char) * nchars_read);
+        if (lineptr_copy== NULL){
+            perror("tsh: memory allocation error");
+            return (-1);
+        }
+        /* copy lineptr to lineptr_copy */
+        strcpy(lineptr_copy, lineptr);
+
+        /* total number of tokens */
+        token = strtok(lineptr, delim);
+
+        while (token != NULL){
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
+        num_tokens++;
+
+        /* Allocate space for the array of strings */
+        argv = malloc(sizeof(char *) * num_tokens);
+
+        /* Store each token in the argv */
+        token = strtok(lineptr_copy, delim);
+
+        for (i = 0; token != NULL; i++){
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
+
+            token = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+
+        printf("%s\n", lineptr);
+
+        /* free the allocated memory */ 
+        free(lineptr);
+    } 
+
+
+    return (0);
+}
