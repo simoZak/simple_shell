8be27aadc69d75785f6b19b1f1d7958dd4e2b58f
simoZak <zakhnini92@gmail.com>
2023-05-25 23:40:03 +0100

    task 0

---
+++ main.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of shell.
+ * @ac: argument count.
+ * @argv: argument variable.
+ * @env: NULL terminated array of strings.
+ *
+ * Return: 0 on success.
+ */
+
+int main(__attribute__((unused))int ac, char **argv, char **env)
+{
+	char *lineptr = NULL, *line_cpy = NULL, *token = NULL;
+	size_t n = 0;
+	ssize_t get;
+	const char *delim = " \n";
+	int ntok = 0, i;
+
+	while (1)
+	{
+		get = getline(&lineptr, &n, stdin);
+		if (get == -1)
+		{
+			free(lineptr);
+			exit(EXIT_SUCCESS);
+		}
+		line_cpy = malloc((sizeof(char) * get + 1));
+		str_cpy(line_cpy, lineptr);
+		token = strtok(lineptr, delim);
+		while (token != NULL)
+		{
+			ntok++;
+			token = strtok(NULL, delim);
+		}
+		free(token);
+		ntok++;
+		argv = malloc(sizeof(char *) * ntok);
+		token = strtok(line_cpy, delim);
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char *) * _strlen(token));
+			str_cpy(argv[i], token);
+			token = strtok(NULL, delim);
+		}
+		free(token);
+		argv[i] = NULL;
+		execute_command(argv, env);
+	}
+	free(lineptr);
+	free(line_cpy);
+	return (0);
+}
